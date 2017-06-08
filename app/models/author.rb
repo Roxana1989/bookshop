66cfb2a43df54cb5b0dc2ea5f471ad02bb7c6413
Dmytro Stepaniuk <stepaniuk@mlsdev.com>
2017-06-09 00:39:54 +0300

    cleaning & refactoring of search by name scopes

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,8 +1,5 @@
 class Author < ActiveRecord::Base
-
   has_and_belongs_to_many :books
+
   has_and_belongs_to_many :articles
-
-  scope :by_name, -> (name) { Author.where("name ~* ?", name) }
-
 end
