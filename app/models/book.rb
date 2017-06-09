90fc6aac2fc830171ea8346c729413dc84fefac4
Dmytro Stepaniuk <stepaniuk@mlsdev.com>
2017-06-09 12:48:11 +0300

    more beautifying

--- app/models/book.rb
+++ app/models/book.rb
@@ -11,13 +11,13 @@ class Book < ApplicationRecord
 
   scope :available, -> { where available: true }
 
-  scope :search_by_name, -> (name_query) { where "name ~* ?", name_query }
+  scope :search_by_name,      -> (name_query)      { where "name ~* ?", name_query }
 
   scope :search_by_publisher, -> (publisher_query) { joins(:publisher).where 'publishers.name ~* ?', publisher_query }
 
-  scope :search_by_genre, -> (genre_query) { joins(:genres).where 'genres.name ~* ?', genre_query }
+  scope :search_by_genre,     -> (genre_query)     { joins(:genres)   .where 'genres.name ~* ?',     genre_query }
 
-  scope :search_by_author, -> (author_query) { joins(:authors).where 'authors.name ~* ?', author_query }
+  scope :search_by_author,    -> (author_query)    { joins(:authors)  .where 'authors.name ~* ?',    author_query }
 
   before_save :setup_has_prime_length_of_name
 
