12fd210d0d117cd5f565ff24ec5d6e17656f771e
Roxana <bachinskaya.roksana@gmail.com>
2017-06-09 14:13:49 +0300

    use query among the scopes

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,13 +3,11 @@ class Article < ApplicationRecord
 
   has_and_belongs_to_many :genres
 
-  validates :name, presence: true
+  scope :search_by_name,      -> (query) { where 'name ~* ?', query }
 
-  scope :search_by_name,      -> (name_query)      { where 'name ~* ?', name_query }
+  scope :search_by_genre,     -> (query) { joins(:genres) .where 'genres.name ~* ?',  query }
 
-  scope :search_by_genre,     -> (genre_query)     { joins(:genres) .where 'genres.name ~* ?',  genre_query }
+  scope :search_by_author,    -> (query) { joins(:authors).where 'authors.name ~* ?', query }
 
-  scope :search_by_author,    -> (author_query)    { joins(:authors).where 'authors.name ~* ?', author_query }
-
-  scope :search_by_publisher, -> (publisher_query) { none } # This column doesn't exist in articles
+  scope :search_by_publisher, -> (query) { none } # This column doesn't exist in articles
 end
